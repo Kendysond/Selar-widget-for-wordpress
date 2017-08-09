af8967b346d2675a968239058a792c482b810097
Douglas Kesi-Ayeba Kendyson <kendyson@kendyson.com>
2017-08-09 23:22:19 +0100

    V1.0

---
+++ readme.txt
@@ -0,0 +1,59 @@
+=== Selar.co Widget ===
+Contributors: kendysond
+Donate link: https://github.com/kendysond
+Tags: Selar,Selar.co, payments, e-commerce, commerce, digital payments,recurrent payments, nigeria, mastercard, visa, target,Naira,payments,verve
+Requires at least: 3.1
+Tested up to: 4.8
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Embed Selar products or profiles to your website. 
+
+== Description ==
+
+This setups a beatiful widget for a selar product or merchant profile.
+
+
+= Suggestions / Feature Request =
+
+If you have suggestions or a new feature request, feel free to get in touch with us via [http://selar.co](http://selar.co)
+
+You can also follow us on Twitter! **[@tryselar](http://twitter.com/tryselar)**
+
+
+== Installation ==
+
+= Automatic Installation =
+*    Login to your WordPress Admin area
+*    Go to "Plugins > Add New" from the left hand menu
+*    In the search box type __Selar.co Widget__
+*    From the search result you will see __Selar.co Widget__ click on __Install Now__ to install the plugin
+*    A popup window will ask you to confirm your wish to install the Plugin.
+*    After installation, activate the plugin.
+
+= Manual Installation =
+1.  Download the plugin zip file
+2.  Login to your WordPress Admin. Click on "Plugins > Add New" from the left hand menu.
+3.  Click on the "Upload" option, then click "Choose File" to select the zip file from your computer. Once selected, press "OK" and press the "Install Now" button.
+4.  Activate the plugin.
+
+
+
+
+== Frequently Asked Questions ==
+
+= What Do I Need To Use The Plugin =
+
+1. You need to open a Selar account on [Selar](https://selar.co)
+
+
+== Changelog ==
+= 1.0 =
+*   First release
+
+== Screenshots ==
+
+
+
+== Other Notes ==
--- selar-embed-widget.php
+++ selar-embed-widget.php
@@ -1,12 +1,16 @@
 <?php
 /*
-Plugin Name: Selar Embed Widget
+Plugin Name: Selar.co Widget
 Plugin URI: https://selar.co/plugins
 Description: Embed a Selar product/profile on your website.
 Author: Kendysond
 Version: 1.0
 Author URI: https://github.com/kendysond
 */
+
+define( 'KDD_SELAR_WIDGET_MAIN_FILE', __FILE__ );
+define( 'KDD_SELAR_WIDGET_VERSION', '1.0' );
+define( 'KDD_SELAR_WIDGET_ENDPOINT', 'http://api.selar.co/v1/' );
 
 class KKD_Selar_Product_Widget extends WP_Widget {
   	public function __construct() {
@@ -17,41 +21,43 @@ class KKD_Selar_Product_Widget extends WP_Widget {
 	    parent::__construct( 'selar_widget', 'Selar Product Widget', $widget_options );
   	}
   	public function form( $instance ) {
-		$title = ! empty( $instance['title'] ) ? $instance['title'] : ''; ?>
+		$code = ! empty( $instance['code'] ) ? $instance['code'] : ''; ?>
 		<p>
-		    <label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
-		    <input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
+		    <label for="<?php echo $this->get_field_id( 'code' ); ?>">Product Code:</label>
+		    <input type="text" id="<?php echo $this->get_field_id( 'code' ); ?>" name="<?php echo $this->get_field_name( 'code' ); ?>" value="<?php echo esc_attr( $code ); ?>" />
 		</p>
 		<?php 
 	}
 	public function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
-		$instance[ 'title' ] = strip_tags( $new_instance[ 'title' ] );
+		$instance[ 'code' ] = strip_tags( $new_instance[ 'code' ] );
 		return $instance;
 	}
   	public function widget( $args, $instance ) {
-		$title = apply_filters('widget_title', $instance[ 'title' ]);
-	  	$blog_title = get_bloginfo('name');
-	  	$tagline = get_bloginfo('description');
-	  	echo $args['before_widget'] . $args['before_title'] . $title . $args['after_title']; 
+  		echo $args['before_widget'] . $args['before_title'] . $args['after_title']; 
+		$code = $instance[ 'code' ];
+	  	$url = KDD_SELAR_WIDGET_ENDPOINT.'products/'.$code;
 	  	
-	  	$url = 'http://api.selar.co/v1/products/demo1';
 		$response = wp_safe_remote_get($url);
-
-		$response_code = wp_remote_retrieve_response_code( $response );
 		$json = wp_remote_retrieve_body($response);
-		print_r(json_decode($json));
-	  	?>
+		$response_object = json_decode($json);
+		$response_code = wp_remote_retrieve_response_code($response);
+		if ($response_code == 200) { ?>
+			<p><strong><?php echo $response_object->name; ?></strong></p>
+			<p><img src="<?php echo $response_object->image; ?>"><strong></strong></p>
+			<p><strong> <a href="<?php echo $response_object->url; ?>" target="_blank"><?php echo $response_object->url; ?></a></strong></p>
 
 
-		<p><strong>Site Name:</strong> <?php echo $blog_title ?></p>
-		<p><strong>Tagline:</strong> <?php echo $tagline ?></p>
+		<?php }else{ ?>
 
-		<?php echo $args['after_widget'];
+			<p><strong><?php echo $response_object->message; ?></strong></p>
+		
+		<?php } echo $args['after_widget'];
 	}
 }
 
 function kkd_selar_register_widget() { 
   register_widget( 'KKD_Selar_Product_Widget' );
+  // register_widget( 'KKD_Selar_Merchant_Widget' );
 }
 add_action( 'widgets_init', 'kkd_selar_register_widget' );
